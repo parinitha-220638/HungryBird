e0a868ba6d2077bd2061c41df61662afd5ebbd79
parinitha-220638 <n220638@rguktn.ac.in>
2026-01-05 11:19:41 +0530

    included js file

---
+++ Class/script.js
@@ -0,0 +1,38 @@
+// alert("welcome to page!!")
+// var $x=15;
+// var x=16;
+// function show(){
+//     let x=10;
+//     console.log(x);
+// }
+// console.log($x);
+// show();
+// var $x=15;
+// var x=16;
+// function show(){
+//     let x=10;
+//     console.log(x);
+// }
+// console.log($x);
+// show();
+
+// let data=10;
+// data="hello";
+// console.log(data)
+// console.log(data)
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(5,9))
+
+// const add=(a,b)=>a+b;
+// console.log(add(5,9));
+
+// age=14
+// if(age>=18){
+//     console.log("You are eligible to vote")
+// }
+// else{
+//     console.log("You are not eligible to vote")
+// }
